c1941c58ccb9ca77be1d3c3331fe20ff808a03a0
Shi Yonghong <shootingsyh@gmail.com>
2012-12-11 11:44:38 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

---
+++ util.php
@@ -0,0 +1,27 @@
+<?php
+
+function error(err, des) {
+  die(
+    json_encode(
+      array(
+        'error' => err,
+        'error_description' => des
+      )));
+}
+
+function check_request() {
+  if (!$_SERVER['HTTPS'] && $_SERVER['HTTP_X_FORWARDED_PROTO'] != 'https') {
+    header('WWW-Authenticate: Bearer, error=invalid_request');
+    error('invalid_request', 'You must use https://');
+  }
+
+  $secret = $_GET['secret'];
+  if ($secret !== '01b416405245af8a0ee4deec6e37ed82') {
+     error('invalid_request', 'Wrong secret');
+  }
+
+  $fbid = $_GET['fbid'];
+  if (!$fbid) {
+    error('invalid_request', 'Missing FBID');
+  }
+}
