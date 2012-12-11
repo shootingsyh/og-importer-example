38ea22b3a4164f499a77c3caa36d2f181ee264dc
Shi Yonghong <shootingsyh@gmail.com>
2012-12-11 14:45:32 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

--- feed.php
+++ feed.php
@@ -4,7 +4,7 @@ require_once('database.php');
 $request = check_feed_request();
 $fbid = $request['user_id']; // user_id in request is fbid.
 $id = get_user_id_by_fbid($fbid); // retriece app's user id from fbid.
-$obj_ids = get_objs_by_user_id($id);
+$obj_ids = get_obj_ids_by_user_id($id);
 $data = array();
 $i = 0;
 foreach ($obj_ids as $obj_id) {
