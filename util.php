c3b4538f6086798c19b2a369b22b1081ba02d4bb
Shi Yonghong <shootingsyh@gmail.com>
2012-12-12 10:22:48 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

--- util.php
+++ util.php
@@ -1,5 +1,5 @@
 <?php
-public function getUrl($path = '/') {
+function getUrl($path = '/') {
   if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1)
     || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http
   ) {
