feba214353ace81a528bdb6f38b044298296cc7c
Shi Yonghong <shootingsyh@gmail.com>
2012-12-12 10:31:24 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

--- util.php
+++ util.php
@@ -1,4 +1,16 @@
 <?php
+
+/**
+ * @return the value at $index in $array or $default if $index is not set.
+ */
+function idx(array $array, $key, $default = null) {
+  return array_key_exists($key, $array) ? $array[$key] : $default;
+}
+
+function he($str) {
+  return htmlentities($str, ENT_QUOTES, "UTF-8");
+}
+
 function getUrl($path = '/') {
   if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1)
     || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http'
