933511e6d81ec61231d65fc460c15d4e9c5c83ac
Shi Yonghong <shootingsyh@gmail.com>
2012-12-12 11:49:51 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

--- import.php
+++ import.php
@@ -16,7 +16,7 @@ if (!$obj_id) {
   echo('<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# actionimporter: http://ogp.me/ns/fb/actionimporter#">');
   echo('<meta property="fb:app_id" content="310812085693919" />');
   echo('<meta property="og:type"   content="actionimporter:obj" />');
-  echo('<meta property="og:url"    content="'.getUrl('/import?obj_id='.$obj_id.'&fbid='.$fbid).'"/>');
+  echo('<meta property="og:url"    content="'.getUrl('/import?obj_id='.$obj_id.'&user_id='.$fbid).'"/>');
   echo('<meta property="og:title"  content="'.$testObj.'" />');
   echo('<meta property="og:image"  content="'.getUrl('/images/sample'.rand(1,5).'.gif').'"/>');
   echo('</head>');
