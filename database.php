48299577ad4e48c2c4472d9ebcfe6ddfd07ff585
Shi Yonghong <shootingsyh@gmail.com>
2012-12-13 11:39:04 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

--- database.php
+++ database.php
@@ -40,12 +40,12 @@ function get_image_by_obj_id($obj_id) {
   return getUrl('/images/city_'.$count.'.jpeg');
 }
 
-city_mapping = array('beijing', 'shanghai', 'seattle', 'dublin', 'menlopark');
+$city_mapping = array('beijing', 'shanghai', 'seattle', 'dublin', 'menlopark');
 
 function get_name_by_obj_id($obj_id) {
   $count = get_count_by_obj_id($obj_id);
   if ($count > 4) { 
     return 'Forgotten Realm'
   }
-  return city_mapping[$count];
+  return $city_mapping[$count];
 }
