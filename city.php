b4a54a4b9e923db4e7525a46926f0350ed97af8b
Shi Yonghong <shootingsyh@gmail.com>
2012-12-19 11:11:02 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

---
+++ city.php
@@ -0,0 +1,26 @@
+<?php
+require_once('util.php');
+require_once('database.php');
+check_public_request();
+$obj_id = $_GET['obj_id'];
+$fbid = $_GET['user_id'];
+if (!$obj_id) {
+  error('invalid_request', 'missing obj id');
+} else {
+  $user_id = get_user_id_by_fbid($fbid);
+  if(!check_obj_id($user_id, $obj_id)) {
+     error('invalid_request', 'invalid obj id '.$obj_id.'. Expecting '.$user_id.'_something');
+  }
+  header("text/html");
+  $testObj = get_name_by_obj_id($obj_id).' city';
+  echo('<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# actionimporter: http://ogp.me/ns/fb/actionimporter#">');
+  echo('<meta property="fb:app_id" content="310812085693919" />');
+  echo('<meta property="og:type"   content="actionimporter:city" />');
+  echo('<meta property="og:url"    content="'.getUrl('/city?obj_id='.$obj_id.'&user_id='.$fbid).'"/>');
+  echo('<meta property="og:title"  content="'.get_name_by_obj_id($obj_id).'" />');
+  echo('<meta property="og:image"  content="'.get_image_by_obj_id($obj_id).'"/>');
+  echo('</head>');
+  echo('<body>'.$testObj.
+          '<br/><img src="'.get_image_by_obj_id($obj_id).'" />'.
+       '</body>');
+}
