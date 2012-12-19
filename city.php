ca5b1c4dd983fd7b8fc2a653a2ac09cd8e269867
Shi Yonghong <shootingsyh@gmail.com>
2012-12-19 11:36:24 -0800

    Summary:
    Task ID: #
    
    Reviewers:
    
    CC:
    
    Test Plan:

--- city.php
+++ city.php
@@ -3,14 +3,9 @@ require_once('util.php');
 require_once('database.php');
 check_public_request();
 $obj_id = $_GET['obj_id'];
-$fbid = $_GET['user_id'];
-if (!$obj_id) {
+if ($obj_id===null) {
   error('invalid_request', 'missing obj id');
 } else {
-  $user_id = get_user_id_by_fbid($fbid);
-  if(!check_obj_id($user_id, $obj_id)) {
-     error('invalid_request', 'invalid obj id '.$obj_id.'. Expecting '.$user_id.'_something');
-  }
   header("text/html");
   $testObj = get_name_by_obj_id($obj_id).' city';
   echo('<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# actionimporter: http://ogp.me/ns/fb/actionimporter#">');
